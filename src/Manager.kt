f0141d81aca3980f518bd94e9d4bdd09d5f27549
PotatoCurry <potatocurry@protonmail.com>
2019-08-31 22:45:58 -0500

    Simplified helper methods

--- src/Manager.kt
+++ src/Manager.kt
@@ -6,7 +6,6 @@ import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
 
@@ -15,29 +14,25 @@ object Manager {
         Boards.selectAll().map(::Board)
     }
 
-    fun insertThread(board: Board, threadTitle: String, threadText: String): InsertStatement<Number> {
-        return transaction {
-            if (board.threads.count() >= 100) {
-                val deleteThread = board.threads.filterNot { it.pinned }.minBy { it.time }?.id
-                Threads.deleteWhere(1) { (id eq deleteThread) }
-                Comments.deleteWhere { (threadId eq deleteThread) }
-            }
-            Threads.insert {
-                it[boardId] = EntityID(board.id, Boards)
-                it[time] = DateTime.now()
-                it[title] = threadTitle
-                it[text] = threadText
-            }
+    fun insertThread(board: Board, threadTitle: String, threadText: String) = transaction {
+        if (board.threads.count() >= 100) {
+            val deleteThread = board.threads.filterNot { it.pinned }.minBy { it.time }?.id
+            Threads.deleteWhere(1) { (id eq deleteThread) }
+            Comments.deleteWhere { (threadId eq deleteThread) }
+        }
+        Threads.insert {
+            it[boardId] = EntityID(board.id, Boards)
+            it[time] = DateTime.now()
+            it[title] = threadTitle
+            it[text] = threadText
         }
     }
 
-    fun insertComment(thread: Thread, commentText: String): InsertStatement<Number> {
-        return transaction {
-            Comments.insert {
-                it[threadId] = EntityID(thread.id, Threads)
-                it[time] = DateTime.now()
-                it[text] = commentText
-            }
+    fun insertComment(thread: Thread, commentText: String) = transaction {
+        Comments.insert {
+            it[threadId] = EntityID(thread.id, Threads)
+            it[time] = DateTime.now()
+            it[text] = commentText
         }
     }
 }
